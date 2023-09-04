80e52de17fe3cc5dfea796e0491e46e8ea7d822e
Asel <139735405+Kazymbekova@users.noreply.github.com>
2023-09-04 19:27:07 +0600

    Update main.ts

--- main.ts
+++ main.ts
@@ -4,24 +4,30 @@ let b: string = "as";
 let c: boolean = false;
 // 2
 function calculateArea(r: number) {
-  return (a = (3.14 * r) ^ 2);
+   let A: number;
+  return (A = (3.14 * r) ^ 2);
 }
+console.log(calculateArea(5));
 //
 function calculateDensity(m: number, v: number) {
   let p: number = m / v;
+  return p;
 }
+console.log(calculateDensity(5, 5));
 // 3
 interface IProduct {
   id: number;
   title: string;
   description: string;
   price: number;
-  colors: string;
+  colors: string[];
 }
 let product: IProduct = {
   id: 1,
   title: "Phone",
   description: "X",
   price: 65000,
-  colors: "black",
+  colors: ["black"],
 };
+console.log(product);
+// чет не разобралась где должен быть результат написала вслепую,сделала как поняла)
